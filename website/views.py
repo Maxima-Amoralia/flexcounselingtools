a9d91b1535ca38855ecb5d71357289812d6ace2e
Maxima-Amoralia <Alexander.ezekiel.chang@gmail.com>
2023-01-15 21:07:33 -0800

    3

--- website/views.py
+++ website/views.py
@@ -15,16 +15,11 @@ import pandas as pd
 #===========================================
 
 
-
-
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note, CA_Activity
 from . import db
 import json
-
-
-
 
 
 
