64790c9e378c4962bd0939dc0bcd7d0a74f2b52d
Maxima-Amoralia <Alexander.ezekiel.chang@gmail.com>
2023-01-15 20:43:02 -0800

    2

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, session, current_app
+from flask import Blueprint, render_template, request, redirect, url_for, session, current_app
 from .models import User
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
