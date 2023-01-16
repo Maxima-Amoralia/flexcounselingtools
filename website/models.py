9dd0b2827bbf13c645d91c8fb57144dffeff56a7
Maxima-Amoralia <Alexander.ezekiel.chang@gmail.com>
2023-01-15 23:13:21 -0800

    u

--- website/models.py
+++ website/models.py
@@ -4,18 +4,10 @@ from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
-    #id = db.Column(db.Integer, primary_key=True)
-    #email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    #first_name = db.Column(db.String(150))
-
-    id = db.Column(db.String(40), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    last_name = db.Column(db.String(150))
-    full_name = db.Column(db.String(150))
-
-
     note = db.relationship('Note')
     ca_activity = db.relationship('CA_Activity')
 
