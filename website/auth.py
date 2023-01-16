b5ae5ee6f7bc73258e54ce17a7d34ff25fc3f5b2
Maxima-Amoralia <Alexander.ezekiel.chang@gmail.com>
2023-01-15 23:52:08 -0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -35,7 +35,7 @@ def login():
 
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
-        redirect_uri=request.base_url + "/callback",
+        redirect_uri="https://www.flexcounselingtools.com/login/callback",
         scope=["openid", "email", "profile"],
     )
     return redirect(request_uri)
