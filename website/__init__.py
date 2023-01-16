5ed6d411adfca281d100867ff9f2a60f938fe1cc
Maxima-Amoralia <Alexander.ezekiel.chang@gmail.com>
2023-01-15 22:46:33 -0800

    1

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+import os
+
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -8,7 +9,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'cokeoriginaltaste'
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
