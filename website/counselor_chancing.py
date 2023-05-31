433635daab3f461a35a4afe2792bd1e2eed76cd0
Maxima-Amoralia <Alexander.ezekiel.chang@gmail.com>
2023-05-30 18:32:58 -0700

    no pkl

--- website/counselor_chancing.py
+++ website/counselor_chancing.py
@@ -107,17 +107,6 @@ def studentdata():
     
     print(collegeList)
 
-    import pickle
-
-    file_name = 'chancing_models.pkl'
-    models = []
-
-    with open(file_name, 'rb') as f:
-        while True:
-            try:
-                models = pickle.load(f)
-            except EOFError:
-                break
 
     output = {
         'id' : student.id,
